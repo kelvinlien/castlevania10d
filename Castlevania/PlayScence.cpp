fcc3e1d1a0d228d3478753360f56c99f385e265f
Kelvin <mr.secret0987@gmail.com>
2020-11-03 00:43:55 +0700

    fix: text and texture path

--- Castlevania/PlayScence.cpp
+++ Castlevania/PlayScence.cpp
@@ -263,7 +263,7 @@ void CPlayScene::Load()
 	}
 	f.close();
 
-	CTextures::GetInstance()->Add(ID_TEX_BBOX, L"textures\\bbox.png", D3DCOLOR_XRGB(255, 255, 255));
+	CTextures::GetInstance()->Add(ID_TEX_BBOX, L"..\\Resources\\Texture\\bbox.png", D3DCOLOR_XRGB(255, 255, 255));
 	DebugOut(L"[INFO] Done loading scene resources %s\n", sceneFilePath);
 	
 }
--- Castlevania/main.cpp
+++ Castlevania/main.cpp
@@ -175,7 +175,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	game->Init(hWnd);
 	game->InitKeyboard();
 
-	game->Load(L"mario-sample.txt");
+	game->Load(L"..\\Resources\\Text\\mario-sample.txt");
 
 	SetWindowPos(hWnd, 0, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, SWP_NOMOVE | SWP_NOOWNERZORDER | SWP_NOZORDER);
 
