cdee9e85a4293e21f71966a4fcf9efdfa4fe49cf
Kelvin <mr.secret0987@gmail.com>
2020-11-11 15:50:39 +0700

    feat: CCandle class based on CFirepot

---
+++ Castlevania/Candle.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "Holder.h"
+#define CANDLE_GRAVITY			0.002f
+#define CANDLE_DEFLECT_SPEED 0.2f
+
+#define	CANDLE_STATE_IDLE	0
+#define CANDLE_STATE_BREAK	100
+
+#define CANDLE_ANI_IDLE	1
+#define CANLDE_ANI_BREAK	2
+
+#define CANDLE_BBOX_WIDTH	16
+#define CANDLE_BBOX_HEIGHT	29
+
+#define CANDLE_BREAK_TIME 100
+
+class CCandle :public CHolder
+{
+	DWORD break_time;
+	ItemType itemType;
+public:
+	CCandle(int itemType);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* colliable_objects = NULL);
+
+	virtual void Render();
+
+	void SetState(int state);
+
+	//Getter & setter
+	void SetItem(int itemType);
+	ItemType GetItemType() { return itemType; }
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+};
+
