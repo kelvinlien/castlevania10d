0e4a54cfe09782a74e4903d43e507a9ce9b31af2
deniliem <bebu1407@gmail.com>
2020-11-18 15:30:50 +0700

    Task complete

--- Castlevania/Enemy.cpp
+++ Castlevania/Enemy.cpp
@@ -3,3 +3,24 @@ CEnemy::CEnemy() :CGameObject()
 {
 	isActive = false;
 }
+void CEnemy::SetItem(int itemType) {
+	ItemType type;
+	switch (itemType)
+	{
+	case 0:
+		type = ITEM_SMALL_HEART;
+		break;
+	case 1:
+		type = ITEM_BIG_HEART;
+		break;
+	case 5:
+		type = ITEM_WHIP_RED;
+		break;
+	case 7:
+		type = ITEM_DAGGER;
+		break;
+	default:
+		break;
+	}
+	this->itemType = type;
+}
