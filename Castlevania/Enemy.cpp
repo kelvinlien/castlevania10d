88734900e88c3ec9f1e0eac96a493c8a0ad84d15
deniliem <bebu1407@gmail.com>
2020-11-27 14:55:01 +0700

    implement stop watch code

--- Castlevania/Enemy.cpp
+++ Castlevania/Enemy.cpp
@@ -2,4 +2,21 @@
 CEnemy::CEnemy() :CGameObject()
 {
 	isActive = false;
+	state = ENEMY_STATE_MOVE;
+	isLock = false;
+}
+void CEnemy::SetState(int state)
+{
+	this->state = state;
+	switch (state)
+	{
+	case ENEMY_STATE_STOP:
+		isLock = true;
+		break;
+	case ENEMY_STATE_MOVE:
+		isLock = false;
+		break;
+	default:
+		break;
+	}
 }
--- Castlevania/Enemy.h
+++ Castlevania/Enemy.h
@@ -1,17 +1,21 @@
 #pragma once
 #include"GameObject.h"
+
+#define ENEMY_STATE_STOP	600
+#define ENEMY_STATE_MOVE	700
 using namespace std;
 class CEnemy: public CGameObject
 {
 protected:
 	bool isActive;
+	bool isLock;
 	int type;
 public:
 	CEnemy();
 	virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom)=0;
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects = NULL)=0;
 	virtual void Render()=0;
-	virtual void SetState(int state) { this->state = state; }
+	virtual void SetState(int state);
 	void SetActive() { if (!isActive) isActive = true; }
 };
 
