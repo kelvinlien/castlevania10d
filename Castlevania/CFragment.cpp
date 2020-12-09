bd917377c1765525597beb7195a275ed93c0dc7c
deniliem <bebu1407@gmail.com>
2020-12-09 13:56:48 +0700

    feat: create class effect fragment

---
+++ Castlevania/CFragment.cpp
@@ -0,0 +1,36 @@
+#include "CFragment.h"
+
+Fragment::Fragment(float x, float y, float nx, int type)
+{
+	this->x = x;
+	this->y = y;
+	vx = nx * FRAGMENT_SPEED_X;
+	vy = -FRAGMENT_SPEED_Y;
+	animation_set=CAnimationSets::GetInstance()->Get()
+	switch (type)
+	{
+	case 0:
+		this->fragmentType = WATER_FRAGMENT;
+		break;
+	case 1:
+		this->fragmentType = RUBBLE_FRAGMENT;
+		break;
+	default:
+		this->fragmentType = WATER_FRAGMENT;
+		break;
+	}
+}
+
+void Fragment::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
+{
+	CGameObject::Update(dt,coObjects);
+	vy += FRAGMENT_GRAVITY;
+
+	x += dx;
+	y += dy;
+}
+
+void Fragment::Render()
+{
+	
+}
