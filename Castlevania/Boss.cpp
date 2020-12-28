d72169001b6464c566ee14322131c9e375793c35
TaiTrien <v.v.t.trien@gmail.com>
2020-12-28 15:08:28 +0700

    feat: basic logic on boss

--- Castlevania/Boss.cpp
+++ Castlevania/Boss.cpp
@@ -5,7 +5,9 @@ CBoss::CBoss()
 {
 	type = 15;
 	isActive = false;
-	isFly = false;
+	isFlying = false;
+	vx = 0;
+	vy = 0;
 }
 
 void CBoss::GetBoundingBox(float &left, float &top, float &right, float &bottom)
@@ -26,55 +28,142 @@ void CBoss::GetBoundingBox(float &left, float &top, float &right, float &bottom)
 	}
 }
 
+void CBoss::SetState(int state)
+{
+	CGameObject::SetState(state);
+	switch (state)
+	{
+	case BOSS_STATE_WAITING:
+		if (isWaiting) return;
+		this->start_x = this->x;
+		this->start_y = this->y;
+		isWaiting = true;
+		isFlying = false;
+		isReverse = false;
+		startWaitTime = GetTickCount();
+		vx = 0;
+		vy = 0;
+		RandomWaitingPos();
+		break;
+	case BOSS_STATE_FLYING:
+		if (isFlying) return;
+
+		isWaiting = false;
+		isFlying = true;
+		isReverse = false;
+		startWaitTime = 0;
+		startFlyTime = GetTickCount();
+		isFlying = true;
+
+		if (this->x < targetPos.x) {
+			vx = BOSS_FLY_SPEED_X;
+		}
+		else 
+			vx = -BOSS_FLY_SPEED_X;
+
+
+		if (this->y >= targetPos.y) {
+			vy = -BOSS_FLY_SPEED_Y;
+		}
+		else
+			vy = BOSS_FLY_SPEED_Y;
+	}
+	
+}
+
+void CBoss::SetTargetPos() {
+	//if (Simon::GetInstance()->x >= this->x)
+	//	isNearSimon = true;
+
+	targetPos.x = Simon::GetInstance()->x;
+	targetPos.y = Simon::GetInstance()->y;
+}
+
+void CBoss::RandomWaitingPos() {
+	srand(time(NULL));
+	int camX = (int)Camera::GetInstance()->GetCamX();
+	int camY = (int)Camera::GetInstance()->GetCamY() ; // 120 is height of board
+
+	waitingPos.x = rand() % (512 - BOSS_FLY_BBOX_WIDTH) + camX;
+	waitingPos.y = rand() % (480 - 160) + 80;
+}
+
 void CBoss::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
 {
 	CGameObject::Update(dt);
-	if (isFly)
-	{
-		vy += BOSS_GRAVITY * dt;
+	if (state == BOSS_STATE_IDLE) {
+		if (!isActive && Simon::GetInstance()->x >= this->x)
+		{
+			isActive = true;
+			SetState(BOSS_STATE_WAITING);
+		}
+		else return;
 	}
 
-	if (Simon::GetInstance()->x >= this->x)
-	{
-		isActive = true;
-	}
 
-	if (isActive)
-	{
-		if (Simon::GetInstance()->x >= this->x)
-			this->nx = 1;
-		else
-			this->nx = -1;
-		Fly();
-	}
-
-	vector<LPCOLLISIONEVENT> coEvents;
-	vector<LPCOLLISIONEVENT> coEventsResult;
-	coEvents.clear();
-
-	if (coEvents.size() == 0)
-	{
-		x += dx;
-		y += dy;
-	}
-	else
-	{
-		float min_tx, min_ty, nx = 0, ny;
-		float rdx = 0;
-		float rdy = 0;
-
-		FilterCollision(coEvents, coEventsResult, min_tx, min_ty, nx, ny, rdx, rdy);
-		// block every object first!
-		x += min_tx * dx + nx * 0.4f;
-		if (ny == -1)
-			y += min_ty * dy + ny * 0.4f;
-		else
-			y += dy;
+	if (isWaiting) {
+		if (GetTickCount() - startWaitTime > BOSS_WAITING_TIME) {
+			SetState(BOSS_STATE_FLYING);
+		}
+		SetTargetPos();
+		flyDistance = sqrt(pow(targetPos.x - this->start_x, 2) + pow(targetPos.y - this->start_y, 2));
 
 	}
 
-	// clean up collision events
-	for (UINT i = 0; i < coEvents.size(); i++) delete coEvents[i];
+	if (isFlying)
+	{
+		DebugOut(L"[TEST]  leftX: %f\n", leftCamX);
+		DebugOut(L"[TEST]  rightX: %f\n", rightCamX);
+		DebugOut(L"[TEST]  topY: %f\n", topCamY);
+		DebugOut(L"[TEST]  bottomY: %f\n", bottomCamY);
+
+		if (!isReverse)
+			fliedDistance = sqrt(pow(this->x - this->start_x, 2) + pow(this->y - this->start_y, 2));
+		else {
+
+			fliedDistance = sqrt(pow(this->x - targetPos.x, 2) + pow(this->y - targetPos.y, 2));
+
+		}
+		if (isOutCamera()) {
+			if (this->x < targetPos.x) {
+				vx = BOSS_FLY_SPEED_X;
+			}
+			else
+				vx = -BOSS_FLY_SPEED_X;
+
+
+			if (this->y >= targetPos.y) {
+				vy = -BOSS_FLY_SPEED_Y;
+			}
+			else
+				vy = BOSS_FLY_SPEED_Y;
+		}
+		if (fliedDistance >= flyDistance && !isReverse) {
+
+			targetPos.x = waitingPos.x;
+			targetPos.y = waitingPos.y;
+			//vx = -vx/2;
+			//vy = -vy/2;
+			flyDistance = sqrt(pow(this->x - targetPos.x, 2) + pow(this->y - targetPos.y, 2));
+
+			//fliedDistance = 0;
+
+			/*vx = -0.15;
+			vy = -0.1;
+
+			if (this->x < targetPos.x) {
+				vx = -vx;
+			}
+
+			if (this->y >= targetPos.y) {
+				vy = -vy;
+			}*/
+		}
+
+		Fly(targetPos);
+		//DebugOut(L"[TEST] distance: %f\n", flyDistance); 
+
+	}
 }
 
 void CBoss::Render()
@@ -87,12 +176,46 @@ void CBoss::Render()
 void CBoss::SetAnimation()
 {
 	ani = BOSS_ANI_IDLE;
-	if (isFly)
-		ani = BOSS_ANI_FLY;
+	if (isActive) ani = BOSS_ANI_FLY;
 }
 
-void CBoss::Fly()
+void CBoss::Fly(D3DXVECTOR2 targetPos)
 {
-	vx = BOSS_FLY_SPEED * this->nx;
-	isFly = true;
+	//if (!isNearSimonByAxisX() && !isNearSimonByAxisY()) {
+		x += dx;
+		y += dy;
+	//}
+
+	/*if (isNearSimonByAxisX()) {
+		y += dy;
+	} 
+
+	if (isNearSimonByAxisY()) {
+		x += dx;
+	}
+*/		
+		/*DebugOut(L"[TEST] fliedDistance: %f\n", fliedDistance);
+		DebugOut(L"[TEST]  flyDistance: %f\n", flyDistance);*/
+		//DebugOut(L"[TEST]  targetPos.x: %f\n", targetPos.x);
+		//DebugOut(L"[TEST]  waitingPos.x: %f\n", waitingPos.x);
+		//DebugOut(L"[TEST]  targetPos.y: %f\n", targetPos.y);
+		//DebugOut(L"[TEST]  waitingPos.y: %f\n", waitingPos.y);
+
+		if (fliedDistance >= flyDistance && targetPos.x == waitingPos.x && targetPos.y == waitingPos.y) {
+			SetState(BOSS_STATE_WAITING);
+			isReverse = true;
+			DebugOut(L"[TEST] isWaiting....\n");
+
+		}
+
+	//if (isNearSimonByAxisX() && isNearSimonByAxisY())
+	//{
+		//isAtTarget = true;
+		/*DebugOut(L"[TEST] targetPos: %f\n", targetPos);
+		DebugOut(L"[TEST] waitingPos: %f\n", waitingPos);*/
+
+		/*if (this->x == waitingPos.x && targetPos.y == waitingPos.y) {
+			SetState(BOSS_STATE_WAITING);
+		}*/
+	//}
 }
--- Castlevania/Boss.h
+++ Castlevania/Boss.h
@@ -1,26 +1,69 @@
 #pragma once
 #include "Enemy.h"
+#include "Camera.h"
+#include "Define.h"
+#include <cmath>
+
 
 #define BOSS_ANI_IDLE	0
 #define BOSS_ANI_FLY	1
-#define BOSS_FLY_SPEED	0.2f
-#define BOSS_GRAVITY	0.0015f
+#define BOSS_FLY_SPEED_X	0.1f
+#define BOSS_FLY_SPEED_Y	0.2f
+
+
+//BBox
 #define BOSS_IDLE_BBOX_WIDTH	32
 #define BOSS_IDLE_BBOX_HEIGHT	32
-#define BOSS_FLY_BBOX_WIDTH	96
+#define BOSS_FLY_BBOX_WIDTH		96
 #define BOSS_FLY_BBOX_HEIGHT	46
+
+//State
+#define BOSS_STATE_IDLE		0
+#define	BOSS_STATE_WAITING	1
+#define	BOSS_STATE_FLYING	2
+#define BOSS_STATE_DEAD		3
+
+//Time
+#define BOSS_WAITING_TIME	1500
+#define BOSS_FLYING_TIME	1500
 
 class CBoss :public CEnemy
 {
+	float leftCamX = 5000;
+	float rightCamX = 5000 + (float)SCREEN_WIDTH;
+	float topCamY = (float)SCREEN_HEIGHT - 120 - 32;
+	float bottomCamY =   600 + (float)SCREEN_HEIGHT - 120 - 32;
 	int ani;
-	bool isFly;
+	int percentFlyToSimon;
+	//flag variables
+	bool isFlying;
+	bool isWaiting;
+	bool isReverse;
+	bool isNearSimon;
+
+	//time variables
+	DWORD startFlyTime;
+	DWORD startWaitTime;
+
+	//target pos
+	D3DXVECTOR2 targetPos;
+	D3DXVECTOR2 waitingPos;
+	
+	//distance 
+	float flyDistance = 0.0f;
+	float fliedDistance = 0.0f;
 
 public:
 	CBoss();
+	void SetState(int state);
 	virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom);
 	virtual void Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects);
 	virtual void Render();
 	void SetAnimation();
-	void Fly();
+	void Fly(D3DXVECTOR2 targetPos);
+	void SetTargetPos();
+	int RandomPercentFlyToSimon() { return  rand() % 3 + 1 ;} // to decide how many percent Boss can fly to Simon
+	void RandomWaitingPos(); //to random waiting pos
+	bool isOutCamera() { return (this->x <= leftCamX || this->x >= rightCamX || this->y <= topCamY || this->y >= bottomCamY); }
 };
 
